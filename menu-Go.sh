1b9726ecfb89a4a9bc55734c394a2cef3a1911a8
NEBULA1978 <102324836+NEBULA1978@users.noreply.github.com>
2023-11-21 22:41:38 +0100

    Create menu-Go.sh
    Opciones a bash

---
+++ menu-Go.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+echo "Bienvenido al script interactivo para el servidor web en Golang."
+
+while true; do
+    echo "Seleccione una opción:"
+    echo "1. Compilar y ejecutar el servidor web"
+    echo "2. Añadir producto"
+    echo "3. Ver productos"
+    echo "4. Redireccionar"
+    echo "5. Mostrar error"
+    echo "6. Salir"
+
+    read -p "Opción: " opcion
+
+    case $opcion in
+        1)
+            go build server.go
+            ./server
+            ;;
+        2)
+            read -p "Nombre del producto: " producto
+            curl -X POST -d "add=$producto" 127.0.0.1:8080/producto
+            ;;
+        3)
+            read -p "Índice del producto: " indice
+            curl 127.0.0.1:8080/producto?prod=$indice
+            ;;
+        4)
+            curl 127.0.0.1:8080/redirect
+            ;;
+        5)
+            curl 127.0.0.1:8080/error
+            ;;
+        6)
+            echo "Saliendo del script. ¡Hasta luego!"
+            exit 0
+            ;;
+        *)
+            echo "Opción no válida. Inténtelo de nuevo."
+            ;;
+    esac
+done
